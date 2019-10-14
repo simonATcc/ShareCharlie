41e0d89d3179701d1c57da88166d73df069a27cc
ChunI Kuo <leftear@msn.com>
2019-10-14 12:29:54 +0800

    commit

--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp3
 {
-  //Modify code for branch1 again
+  //Modify code for branch1 really to branch1
   public partial class Form1 : Form
   {
     public Form1()
