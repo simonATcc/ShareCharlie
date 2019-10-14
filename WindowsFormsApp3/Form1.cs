07cb28981a5bcb80839d0eeaf9f4af6d11eda5e4
ChunI Kuo <leftear@msn.com>
2019-10-14 12:20:21 +0800

    update

--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp3
 {
-  //Modify code for branch1
+  //Modify code for branch1 again
   public partial class Form1 : Form
   {
     public Form1()
